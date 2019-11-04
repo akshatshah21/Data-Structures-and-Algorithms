9a64db49c9b42656d1ced407903c459d392e2809
Akshat Shah <akshatshah_21@yahoo.in>
2019-11-04 07:43:59 +0000

    kruskal done

--- kruskal.cpp
+++ kruskal.cpp
@@ -4,23 +4,27 @@
 #include<vector>
 using namespace std;
 #define MAX_NODES 20
-int djarr[MAX_NODES];
-
-int do_union(int x, int y) {
+vector<int> djarr;
+void do_union(int x, int y) {
 	if(x < y)	swap(x,y);
 	int temp = djarr[x];
-	// search for all temp, replace with djarr[y]
+	for(int i=0;i<djarr.size();i++) {
+		if(djarr[i] == temp)	djarr[i] = djarr[y];
+	}
 }
-
 int find(int x, int y) {
 	return djarr[x] == djarr[y] ? true : false;
 }
-
 bool comp(pair<pair<int, int>, int> e1, pair<pair<int, int>, int> e2) {
-	return e1.second > e2.second;
+	return e1.second < e2.second;
 }
-
 int main() {
+	int v;
+	cout << "Enter number of nodes:";
+	cin >> v;
+	for(int i=0;i<v;i++) {
+		djarr.push_back(i);
+	}	
 	int n;
 	cout << "Enter number of edges:";
 	cin >> n;
@@ -31,15 +35,19 @@ int main() {
 		graph.push_back({{v1, v2}, weight});
 	}
 	sort(graph.begin(), graph.end(), comp);
-
+	int min_cost = 0;
 	vector<pair<pair<int, int>, int>> mst;
 	for(int i=0; i<n; i++) {
-		if(!find(graph[i].first.first, graph[i].first.second)) {
+		if(!find(graph[i].first.first, graph[i].first.second)) { 
 			do_union(graph[i].first.first, graph[i].first.second);
 			min_cost += graph[i].second;
+			mst.push_back({{graph[i].first.first, graph[i].first.second}, graph[i].second});
 		}
 	}
-
 	cout << min_cost << endl;
-	// Print mst
+	cout << "N1 N2 Weight\n";
+	for(int i=0;i<mst.size();i++) {
+		cout << mst[i].first.first << ' ' << mst[i].first.second << ' ' << mst[i].second << endl;
+	}
+	return 0;
 }
