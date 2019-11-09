34f17f49b1a60aea7114a98fed9de278dfce0605
Akshat Shah <akshatshah_21@yahoo.in>
2019-11-09 18:48:48 +0530

    corrected kruskal_edge_class.cpp

--- kruskal_edge_class.cpp
+++ kruskal_edge_class.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
-#include<utility>
 #include<algorithm>
-#include<vector>
 using namespace std;
 #define MAX_NODES 20
-vector<int> djarr;
+int ds[MAX_NODES];
+int v, n;
 class Edge {
 public:
 	int src, dest, weight;
@@ -18,28 +17,22 @@ public:
 bool sort_edge(Edge e1, Edge e2) {
 	return e1.weight < e2.weight;
 }
-
 void do_union(int x, int y) {
 	if(x < y)	swap(x,y);
-	int temp = djarr[x];
-	for(int i=0;i<djarr.size();i++) {
-		if(djarr[i] == temp)	djarr[i] = djarr[y];
+	int temp = ds[x];
+	for(int i=0;i<=v;i++) {
+		if(ds[i] == temp)	ds[i] = ds[y];
 	}
 }
 int find(int x, int y) {
-	return djarr[x] == djarr[y] ? true : false;
-}
-bool comp(pair<pair<int, int>, int> e1, pair<pair<int, int>, int> e2) {
-	return e1.second < e2.second;
+	return ds[x] == ds[y] ? true : false;
 }
 int main() {
-	int v;
  	cout << "Enter number of nodes:";
 	cin >> v;
 	for(int i=0;i<=v;i++) { // <= v for it to work with first node as 0 or 1
-		djarr.push_back(i);
+		ds[i] = i;
 	}
-	int n;
  	cout << "Enter number of edges:";
 	cin >> n;
 	Edge graph[n];
@@ -60,10 +53,8 @@ int main() {
 			mst[count++] = Edge(graph[i].src, graph[i].dest, graph[i].weight);
 		}
 	}
-	cout << min_cost << endl;
- 	cout << "N1 N2 Weight\n";
- 	for(int i=0;i<v-1;i++) {
+	cout << min_cost << "\nN1 N2 Weight\n";
+ 	for(int i=0;i<v-1;i++)
  		cout << mst[i].src << ' ' << mst[i].dest << ' ' << mst[i].weight << endl;
- 	}
 	return 0;
 }
