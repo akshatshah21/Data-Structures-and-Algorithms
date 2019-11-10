a0db95c421197338ebb14b3e3cd05df25d9abdf9
Akshat Shah <akshatshah_21@yahoo.in>
2019-11-10 10:38:08 +0530

    priorityQusingBHeap.cpp works

--- priorityQusingBHeap.cpp
+++ priorityQusingBHeap.cpp
@@ -14,13 +14,11 @@ void enqueue(int data) {
 	}
 	arr[ptr++] = data;
 	int pos = ptr - 1;
-	while(arr[pos] < arr[(pos-1)/2] && pos > 0) {
-		// cout << "here\n";
-		swap(arr[pos], arr[pos/2]);
-		pos /= 2;
+	while(arr[pos] < arr[(pos-1)/2] && pos > 0) {	// new element floating to its place
+		swap(arr[pos], arr[(pos-1)/2]);
+		pos = (pos-1) / 2;
 	}
 }
- 
 int get_front() {
 	if(ptr == 0) {
 		cout << "Priority Queue empty. Returning -1\n";
@@ -28,7 +26,6 @@ int get_front() {
 	}
 	return arr[0];
 }
-
 int dequeue() {
 	if(ptr == 0) {
 		cout << "Priority Queue empty. Returning -1\n";
@@ -37,20 +34,19 @@ int dequeue() {
 	int temp = arr[0];
 	arr[0] = arr[--ptr];
 	int pos = 0;
-	while((arr[pos] > arr[2*(pos+1)] || arr[pos] > arr[2*(pos + 1)-1]) && (2*(pos + 1)-1 < ptr)) {
-		// cout << "here\n";
-		if(arr[2*(pos+1)] > arr[2*(pos+1)-1]) {
-			swap(arr[pos], arr[2*(pos+1)]);
-			pos = 2*(pos+1);
+	while(arr[pos] > arr[2*pos+1] || arr[pos] > arr[2*pos+2]) {	// root element sinking to its place
+		if(arr[2*pos+1] < arr[2*pos+2] && (2*pos+1 < ptr)) { // left > right
+			swap(arr[pos], arr[2*pos+1]);
+			pos = 2*pos+1;
 		}
-		else {
-			swap(arr[pos], arr[2*(pos+1)-1]);
-			pos = 2*(pos+1)-1;
+		else if(2*pos+2 < ptr){ // right >= left
+			swap(arr[pos], arr[2*pos+2]);
+			pos = 2*pos+2;
 		}
+		else	break;
 	}
 	return temp;
 }
-
 void print() {
 	if(ptr == 0) {
 		cout << "Priority Queue is empty\n";
@@ -59,7 +55,6 @@ void print() {
 	for(int i=0;i<ptr;i++)	cout << arr[i] << ' ';
 	cout << endl;
 }
-
 int main() {
 	cout << "1-Enqueue\t2-Get Front\t3-Dequeue\n4-Print\t5-Exit\n";
 	int op=-1;
