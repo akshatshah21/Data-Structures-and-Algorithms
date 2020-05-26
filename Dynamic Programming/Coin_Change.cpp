46985ad0230d481207cc2a1b349c840e5c74f3ea
Akshat Shah <akshatshah_21@yahoo.in>
2020-05-26 15:26:38 +0530

    Added construction of solution to Coin Change

--- Dynamic Programming/Coin_Change.cpp
+++ Dynamic Programming/Coin_Change.cpp
@@ -15,15 +15,30 @@ int main() {
 	// Backward dp
 	int dp[S+1]; // dp[i] = min number of coins to get total i
 	dp[0] = 0;
+	vector<int> coins;
+	int transition_coin[S+1]={0};
 	for(int i=1;i<=S;i++) {
-		int minc = INT_MAX;
+		int minc = INT_MAX, mini=-1;
 		for(int j=0;j<n;j++) {
-			if(i-v[j]>=0)
-				minc = min(minc, dp[i-v[j]]+1);
+			if(i-v[j]>=0 && minc > dp[i-v[j]]+1) {
+				minc = dp[i-v[j]]+1;
+				mini = j;
+			}
 		}
-		dp[i] = (minc==INT_MAX?-1:minc);
+		if(minc != INT_MAX) {
+			dp[i] = minc;
+			transition_coin[i] = v[mini];
+		}
+		else
+			dp[i] = -1;
 	}
-	cout << dp[S] << "\n";
+	cout << dp[S] << "\n"; // -1 => not possible
+	if(dp[S] != -1)
+		for(int i=S, j=0;i>0;j++) {
+			cout << transition_coin[i] << " ";
+			i -= transition_coin[i];
+		}
+	cout << "\n";
 	
 	// Forward dp
 	int dp2[S+1];
@@ -36,6 +51,7 @@ int main() {
 				dp2[i+v[j]] = min(dp2[i+v[j]], dp2[i]+1);
 		}
 	}
-	cout << dp2[S] << "\n";
+	cout << dp2[S] << "\n"; // INT_MAX => not possible
+
 
 }
