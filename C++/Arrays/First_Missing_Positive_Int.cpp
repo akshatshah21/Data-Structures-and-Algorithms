0c16d0790706bcf5a7e89e25395238574630fb40
Akshat Shah <akshatshah_21@yahoo.in>
2021-01-26 16:37:45 +0530

    Leetcode solution replaces raw? solution

--- C++/Arrays/First_Missing_Positive_Int.cpp
+++ C++/Arrays/First_Missing_Positive_Int.cpp
@@ -3,34 +3,40 @@
  * integer in linear time and constant space. In other words,
  * find the lowest positive integer that does not exist in the
  * array. The array can contain duplicates and negative numbers
- * Input: 
+ * Input:
  * n
  * a1 a2 a3 ... an
  */
 
-#include<iostream>
-using namespace std;
+/**
+ * LeetCode: https://leetcode.com/problems/first-missing-positive
+*/
 
-int f(int a[], int n) {
-	for(int i=0; i<n; i++) {
-		if(a[i] <= 0 || a[i] > n || a[i] == i+1) continue;
-		if(a[i] == a[a[i]-1]) a[i] = -a[i];
-		else if(a[a[i]-1] <= 0 || a[a[i]-1] > n) a[a[i]-1] = a[i];
-		else {
-			swap(a[i], a[a[i]-1]);
-			i--;
-		}
-	}
-	for(int i=0; i<n; i++) {
-		if(a[i] != i+1) return (i+1);
-	}
-	return n+1;
-}
-
-int main() {
-	int n;
-	cin >> n;
-	int a[n];
-	for(int i=0; i<n; i++) cin >> a[i];
-	cout << f(a, n) << '\n';
-}
+class Solution {
+private:
+    int n;
+    bool valid(int x) {
+        return (x > 0 && x <= n);
+    }
+public:
+    int firstMissingPositive(vector<int>& a) {
+        n = a.size();
+        for(int i=0; i<n; i++) {
+            if(valid(a[i])) {
+                if(a[i] == i+1) continue;
+                if(a[a[i]-1] == a[i]) {
+                    a[i] = 0;
+                } else if(!valid(a[a[i]-1])) {
+                    a[a[i]-1] = a[i];
+                } else {
+                    swap(a[i], a[a[i]-1]);
+                    i--;
+                }
+            }
+        }
+        for(int i=0; i<n; i++) {
+            if(a[i] != i+1) return (i+1);
+        }
+        return n+1;
+    }
+};
